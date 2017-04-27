cf56f5669e95a7efdf217be4669862db9e70fadd
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2017-04-27 19:48:12 +0300

    TimestampFactory class added

---
+++ Source/Juhta.Net/Common/TimestampFactory.cs
@@ -0,0 +1,97 @@
+﻿
+//
+// Juhta.NET, Copyright (c) 2017 Juha Lähteenmäki
+//
+// This source code may be used, modified and distributed under the terms of
+// the MIT license. Please refer to the LICENSE.txt file for details.
+//
+
+using System;
+using System.Diagnostics;
+
+namespace Juhta.Net.Common
+{
+    /// <summary>
+    /// Defines a class for generating unique 21-digit timestamps. Timestamps will be returned in Coordinated Universal
+    /// Time (UTC). This class is thread-safe.
+    /// </summary>
+    public class TimestampFactory
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public TimestampFactory()
+        {
+            m_created = DateTime.UtcNow;
+
+            m_stopwatch = new Stopwatch();
+
+            m_stopwatch.Start();
+
+            m_syncLock = new object();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Gets a 21-digit unique timestamp.
+        /// </summary>
+        /// <returns>Returns the unique timestamp that was given to the calling thread.</returns>
+        public string GetUniqueTimestamp()
+        {
+            DateTime now;
+            string timestamp;
+            bool isUnique;
+
+            do
+            {
+                now = m_created.Add(m_stopwatch.Elapsed);
+
+                timestamp = String.Format("{0:0000}{1:00}{2:00}{3:00}{4:00}{5:00}", now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+
+                timestamp += String.Format("{0:0000000}", now.Ticks % 10000000);
+
+                lock(m_syncLock)
+                {
+                    isUnique = String.Compare(timestamp, m_lastTimestamp) > 0;
+
+                    if (isUnique)
+                        m_lastTimestamp = timestamp;
+                }
+            }
+            while (!isUnique);
+
+            return(timestamp);
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        /// <summary>
+        /// Specifies the time when this <see cref="TimestampFactory"/> instance was created.
+        /// </summary>
+        private DateTime m_created;
+
+        /// <summary>
+        /// Specifies the last timestamp that was returned from this <see cref="TimestampFactory"/> instance.
+        /// </summary>
+        private string m_lastTimestamp;
+
+        /// <summary>
+        /// Measures the elapsed time since this <see cref="TimestampFactory"/> instance was created.
+        /// </summary>
+        private Stopwatch m_stopwatch;
+
+        /// <summary>
+        /// Specifies a synchronization object for serializing concurrent access.
+        /// </summary>
+        private object m_syncLock;
+
+        #endregion
+    }
+}
---
+++ Tests/Juhta.Net.Tests/Common/TimestampFactoryTests.cs
@@ -0,0 +1,90 @@
+﻿
+using Juhta.Net.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Juhta.Net.Tests.Common
+{
+    [TestClass]
+    public class TimestampFactoryTests
+    {
+        #region Test Methods
+
+        [TestMethod]
+        public void GetUniqueTimestamp_MultipleThreadStress_ShouldReturn()
+        {
+            Thread[] threads;
+            int threadCount = c_threadCount;
+            ThreadParam threadParam = new ThreadParam
+            {
+                LoopCount = c_loopCount,
+                SyncLock = new object(),
+                TimestampFactory = new TimestampFactory(),
+                Timestamps = new Dictionary<string, string>()
+            };
+
+            threads = new Thread[threadCount];
+
+            for (int i = 0; i < threadCount; i++)
+                threads[i] = new Thread(new ParameterizedThreadStart(ThreadMain));
+
+            for (int i = 0; i < threadCount; i++)
+                threads[i].Start(threadParam);
+
+            for (int i = 0; i < threadCount; i++)
+                threads[i].Join();
+
+            Assert.AreEqual<int>(c_threadCount * c_loopCount, threadParam.Timestamps.Count);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static void ThreadMain(object threadParamObj)
+        {
+            ThreadParam threadParam = (ThreadParam)threadParamObj;
+            string timestamp;
+
+            for (int i = 0; i < threadParam.LoopCount; i++)
+            {
+                timestamp = threadParam.TimestampFactory.GetUniqueTimestamp();
+
+                lock (threadParam.SyncLock)
+                {
+                    threadParam.Timestamps.Add(timestamp, timestamp);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Private Types
+
+        private class ThreadParam
+        {
+            #region Public Properties
+
+            public int LoopCount {get; set;}
+
+            public object SyncLock {get; set;}
+
+            public TimestampFactory TimestampFactory {get; set;}
+
+            public Dictionary<string, string> Timestamps {get; set;}
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Private Constants
+
+        private const int c_loopCount = 10000;
+
+        private const int c_threadCount = 200;
+
+        #endregion
+    }
+}
