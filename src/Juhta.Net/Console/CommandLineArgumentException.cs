57155536c369b7678f3d46f870fb596b00973e99
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-03-20 20:52:16 +0200

    CommandLineArgumentException class added

---
+++ src/Juhta.Net/Console/CommandLineArgumentException.cs
@@ -0,0 +1,37 @@
+
+//
+// Juhta.NET, Copyright (c) 2017 Juha Lähteenmäki
+//
+// This source code may be used, modified and distributed under the terms of
+// the MIT license. Please refer to the LICENSE.txt file for details.
+//
+
+using System;
+
+namespace Juhta.Net.Console
+{
+    /// <summary>
+    /// This exception will be thrown when an invalid argument is encountered in command line parsing.
+    /// </summary>
+    public class CommandLineArgumentException : Exception
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="message">Specifies an error message.</param>
+        public CommandLineArgumentException(string message) : base(message)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="innerException">Specifies an inner exception.</param>
+        public CommandLineArgumentException(string message, Exception innerException) : base(message, innerException)
+        {}
+
+        #endregion
+    }
+}
