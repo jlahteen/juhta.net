d230ebebf6cd162f63df4d6beaaa562298f06767
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-03-20 20:55:25 +0200

    Old command line parsing model classes commented or removed

--- src/Juhta.Net/Console/CommandLineArg.cs
+++ src/Juhta.Net/Console/CommandLineArg.cs
@@ -1,112 +1,112 @@
 
-//
-// Juhta.NET, Copyright (c) 2017 Juha Lähteenmäki
-//
-// This source code may be used, modified and distributed under the terms of
-// the MIT license. Please refer to the LICENSE.txt file for details.
-//
+////
+//// Juhta.NET, Copyright (c) 2017 Juha Lähteenmäki
+////
+//// This source code may be used, modified and distributed under the terms of
+//// the MIT license. Please refer to the LICENSE.txt file for details.
+////
 
-using Juhta.Net.Extensions;
-using Juhta.Net.Validators;
+//using Juhta.Net.Extensions;
+//using Juhta.Net.Validators;
 
-namespace Juhta.Net.Console
-{
-    /// <summary>
-    /// Defines an abstract base class for command line arguments.
-    /// </summary>
-    public abstract class CommandLineArg
-    {
-        #region Public Methods
+//namespace Juhta.Net.Console
+//{
+//    /// <summary>
+//    /// Defines an abstract base class for command line arguments.
+//    /// </summary>
+//    public abstract class CommandLineArg
+//    {
+//        #region Public Methods
 
-        /// <summary>
-        /// Validates the value of this CommandLineArg object through a specified IStringValidator instance.
-        /// </summary>
-        /// <param name="validator">Specifies an IStringValidator instance.</param>
-        public void ValidateValue(IStringValidator validator)
-        {
-            if (m_value == null)
-                throw new CommandLineArgException(LibraryMessages.Error044.FormatMessage(m_rawArg));
+//        /// <summary>
+//        /// Validates the value of this CommandLineArg object through a specified IStringValidator instance.
+//        /// </summary>
+//        /// <param name="validator">Specifies an IStringValidator instance.</param>
+//        public void ValidateValue(IStringValidator validator)
+//        {
+//            if (m_value == null)
+//                throw new CommandLineArgException(LibraryMessages.Error044.FormatMessage(m_rawArg));
 
-            try
-            {
-                validator.Validate(m_value);
-            }
+//            try
+//            {
+//                validator.Validate(m_value);
+//            }
 
-            catch (ValidationException ex)
-            {
-                if (m_value == m_rawArg)
-                    throw new CommandLineArgException(LibraryMessages.Error045.FormatMessage(m_value), ex);
-                else
-                    throw new CommandLineArgException(LibraryMessages.Error047.FormatMessage(m_value), ex);
-            }
-        }
+//            catch (ValidationException ex)
+//            {
+//                if (m_value == m_rawArg)
+//                    throw new CommandLineArgException(LibraryMessages.Error045.FormatMessage(m_value), ex);
+//                else
+//                    throw new CommandLineArgException(LibraryMessages.Error047.FormatMessage(m_value), ex);
+//            }
+//        }
 
-        /// <summary>
-        /// Validates the value of this CommandLineArg object with a specified regular expression.
-        /// </summary>
-        /// <param name="regex">Specifies a regular expression.</param>
-        public void ValidateValue(string regex)
-        {
-            if (m_value == null)
-                throw new CommandLineArgException(LibraryMessages.Error044.FormatMessage(m_rawArg));
+//        /// <summary>
+//        /// Validates the value of this CommandLineArg object with a specified regular expression.
+//        /// </summary>
+//        /// <param name="regex">Specifies a regular expression.</param>
+//        public void ValidateValue(string regex)
+//        {
+//            if (m_value == null)
+//                throw new CommandLineArgException(LibraryMessages.Error044.FormatMessage(m_rawArg));
 
-            if (!m_value.IsMatch(regex))
-                if (m_value == m_rawArg)
-                    throw new CommandLineArgException(LibraryMessages.Error046.FormatMessage(m_value, regex));
-                else
-                    throw new CommandLineArgException(LibraryMessages.Error048.FormatMessage(m_value, regex));
-        }
+//            if (!m_value.IsMatch(regex))
+//                if (m_value == m_rawArg)
+//                    throw new CommandLineArgException(LibraryMessages.Error046.FormatMessage(m_value, regex));
+//                else
+//                    throw new CommandLineArgException(LibraryMessages.Error048.FormatMessage(m_value, regex));
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Public Properties
+//        #region Public Properties
 
-        /// <summary>
-        /// Gets the raw command line argument specified by this CommandLineArg object.
-        /// </summary>
-        public string RawArg
-        {
-            get {return(m_rawArg);}
-        }
+//        /// <summary>
+//        /// Gets the raw command line argument specified by this CommandLineArg object.
+//        /// </summary>
+//        public string RawArg
+//        {
+//            get {return(m_rawArg);}
+//        }
 
-        /// <summary>
-        /// Gets the (possibly parsed) value of the command line argument specified by this CommandLineArg object.
-        /// </summary>
-        public string Value
-        {
-            get {return(m_value);}
-        }
+//        /// <summary>
+//        /// Gets the (possibly parsed) value of the command line argument specified by this CommandLineArg object.
+//        /// </summary>
+//        public string Value
+//        {
+//            get {return(m_value);}
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Protected Constructors
+//        #region Protected Constructors
 
-        /// <summary>
-        /// Initializes a new instance.
-        /// </summary>
-        /// <param name="rawArg">Specifies a value for the <see cref="RawArg"/> property.</param>
-        /// <param name="value">Specifies a value for the <see cref="Value"/> property.</param>
-        protected CommandLineArg(string rawArg, string value)
-        {
-            m_rawArg = rawArg;
+//        /// <summary>
+//        /// Initializes a new instance.
+//        /// </summary>
+//        /// <param name="rawArg">Specifies a value for the <see cref="RawArg"/> property.</param>
+//        /// <param name="value">Specifies a value for the <see cref="Value"/> property.</param>
+//        protected CommandLineArg(string rawArg, string value)
+//        {
+//            m_rawArg = rawArg;
 
-            m_value = value;
-        }
+//            m_value = value;
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Protected Fields
+//        #region Protected Fields
 
-        /// <summary>
-        /// Stores the <see cref="RawArg"/> property.
-        /// </summary>
-        protected string m_rawArg;
+//        /// <summary>
+//        /// Stores the <see cref="RawArg"/> property.
+//        /// </summary>
+//        protected string m_rawArg;
 
-        /// <summary>
-        /// Stores the <see cref="Value"/> property.
-        /// </summary>
-        protected string m_value;
+//        /// <summary>
+//        /// Stores the <see cref="Value"/> property.
+//        /// </summary>
+//        protected string m_value;
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
--- src/Juhta.Net/Console/CommandLineOption.cs
+++ src/Juhta.Net/Console/CommandLineOption.cs
@@ -1,53 +1,53 @@
 
-//
-// Juhta.NET, Copyright (c) 2017 Juha Lähteenmäki
-//
-// This source code may be used, modified and distributed under the terms of
-// the MIT license. Please refer to the LICENSE.txt file for details.
-//
+////
+//// Juhta.NET, Copyright (c) 2017 Juha Lähteenmäki
+////
+//// This source code may be used, modified and distributed under the terms of
+//// the MIT license. Please refer to the LICENSE.txt file for details.
+////
 
-namespace Juhta.Net.Console
-{
-    /// <summary>
-    /// Defines a class that represents a command line option. At the command line level, command line options consist
-    /// of a prefix, name and an optional value, which is separated by a name-value separator from the name part.
-    /// </summary>
-    public class CommandLineOption : CommandLineArg
-    {
-        #region Public Properties
+//namespace Juhta.Net.Console
+//{
+//    /// <summary>
+//    /// Defines a class that represents a command line option. At the command line level, command line options consist
+//    /// of a prefix, name and an optional value, which is separated by a name-value separator from the name part.
+//    /// </summary>
+//    public class CommandLineOption : CommandLineArg
+//    {
+//        #region Public Properties
 
-        /// <summary>
-        /// Gets the name of the command line option specified by this CommandLineOption object.
-        /// </summary>
-        public string Name
-        {
-            get {return(m_name);}
-        }
+//        /// <summary>
+//        /// Gets the name of the command line option specified by this CommandLineOption object.
+//        /// </summary>
+//        public string Name
+//        {
+//            get {return(m_name);}
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Internal Constructors
+//        #region Internal Constructors
 
-        /// <summary>
-        /// Initializes a new instance.
-        /// </summary>
-        /// <param name="rawArg">Species a value for the <see cref="CommandLineArg.RawArg"/> property.</param>
-        /// <param name="name">Species a value for the <see cref="Name"/> property.</param>
-        /// <param name="value">Species a value for the <see cref="CommandLineArg.Value"/> property.</param>
-        internal CommandLineOption(string rawArg, string name, string value) : base(rawArg, value)
-        {
-            m_name = name;
-        }
+//        /// <summary>
+//        /// Initializes a new instance.
+//        /// </summary>
+//        /// <param name="rawArg">Species a value for the <see cref="CommandLineArg.RawArg"/> property.</param>
+//        /// <param name="name">Species a value for the <see cref="Name"/> property.</param>
+//        /// <param name="value">Species a value for the <see cref="CommandLineArg.Value"/> property.</param>
+//        internal CommandLineOption(string rawArg, string name, string value) : base(rawArg, value)
+//        {
+//            m_name = name;
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Private Fields
+//        #region Private Fields
 
-        /// <summary>
-        /// Stores the <see cref="Name"/> property.
-        /// </summary>
-        private string m_name;
+//        /// <summary>
+//        /// Stores the <see cref="Name"/> property.
+//        /// </summary>
+//        private string m_name;
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
