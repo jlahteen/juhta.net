8ee752943e93bd704fbca91ac8ca0c201a149f20
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-04-28 20:25:44 +0300

    DirectoryPathValidator and FilePathValidator tests added

---
+++ tests/Juhta.Net.Tests/Validators/DirectoryPathValidatorTests.cs
@@ -0,0 +1,111 @@
+﻿
+using Juhta.Net.Common;
+using Juhta.Net.Validators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Juhta.Net.Tests.Validators
+{
+    [TestClass]
+    public class DirectoryPathValidatorTests
+    {
+        #region Test Methods
+
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
+        public void Validate_InvalidDirectoryPath_DirectoryNameEndsWithDot_ShouldThrowValidationException()
+        {
+            DirectoryPathValidator validator = new DirectoryPathValidator();
+
+            validator.Validate(OSConvert(@"C:\My Documents\Shopping."));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
+        public void Validate_InvalidDirectoryPath_DirectoryNameEndsWithSpace_ShouldThrowValidationException()
+        {
+            DirectoryPathValidator validator = new DirectoryPathValidator();
+
+            validator.Validate(OSConvert(@"C:\My Documents\Shopping\MyDoc.docx "));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
+        public void Validate_InvalidDirectoryPath_IllegalCharacterInDirectoryName_ShouldThrowValidationException()
+        {
+            DirectoryPathValidator filePathValidator = new DirectoryPathValidator();
+
+            filePathValidator.Validate(OSConvert(@"C:\My Documents\Shopping\MyDocX"));
+        }
+
+        [TestMethod]
+        public void Validate_ValidDirectoryPath1_ShouldReturn()
+        {
+            DirectoryPathValidator filePathValidator = new DirectoryPathValidator();
+
+            filePathValidator.Validate(OSConvert(@"C:\My Documents\Shopping"));
+        }
+
+        [TestMethod]
+        public void Validate_ValidDirectoryPath2_ShouldReturn()
+        {
+            DirectoryPathValidator filePathValidator = new DirectoryPathValidator();
+
+            filePathValidator.Validate(OSConvert(@"C:\My Documents\Shopping\MyDoc.docx"));
+        }
+
+        [TestMethod]
+        public void Validate_ValidDirectoryPath3_ShouldReturn()
+        {
+            DirectoryPathValidator filePathValidator = new DirectoryPathValidator();
+
+            filePathValidator.Validate(OSConvert(@"C:\My Documents\.\Shopping\MyDoc.docx"));
+        }
+
+        [TestMethod]
+        public void Validate_ValidDirectoryPath4_ShouldReturn()
+        {
+            DirectoryPathValidator filePathValidator = new DirectoryPathValidator();
+
+            filePathValidator.Validate(OSConvert(@"C:\My Documents\..\Shopping\MyDoc.docx"));
+        }
+
+        [TestMethod]
+        public void Validate_ValidDirectoryPath5_ShouldReturn()
+        {
+            DirectoryPathValidator validator = new DirectoryPathValidator();
+
+            validator.Validate(OSConvert(@"MyDoc.docx"));
+        }
+
+        [TestMethod]
+        public void Validate_ValidDirectoryPath6_ShouldReturn()
+        {
+            DirectoryPathValidator validator = new DirectoryPathValidator();
+
+            validator.Validate(OSConvert(@"MyDoc"));
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static string OSConvert(string path)
+        {
+            if (OSInfo.IsWindows)
+                path = path.Replace('X', '<');
+            else
+            {
+                path = path.Replace("C:", "");
+
+                path = path.Replace('/', Path.DirectorySeparatorChar);
+
+                path = path.Replace('X', '\0');
+            }
+
+            return(path);
+        }
+
+        #endregion
+    }
+}
--- tests/Juhta.Net.Tests/Validators/FilePathValidatorTests.cs
+++ tests/Juhta.Net.Tests/Validators/FilePathValidatorTests.cs
@@ -1,12 +1,8 @@
 ﻿
+using Juhta.Net.Common;
 using Juhta.Net.Validators;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Juhta.Net.Tests.Validators
 {
@@ -17,61 +13,124 @@ namespace Juhta.Net.Tests.Validators
 
         [TestMethod]
         [ExpectedException(typeof(ValidationException))]
-        public void Validate_InvalidFilePath1_ShouldThrowValidationException()
+        public void Validate_InvalidFilePath_DirectoryNameEndsWithDot_ShouldThrowValidationException()
         {
-            FilePathValidator filePathValidator = new FilePathValidator();
+            FilePathValidator validator = new FilePathValidator();
 
-            filePathValidator.Validate(@"C:\My Documents\Shopping\MyD<oc.docx");
+            validator.Validate(OSConvert(@"C:\My Documents.\Shopping\MyDoc.docx"));
         }
 
         [TestMethod]
         [ExpectedException(typeof(ValidationException))]
-        public void Validate_InvalidFilePath2_ShouldThrowValidationException()
+        public void Validate_InvalidFilePath_DirectoryNameEndsWithSpace_ShouldThrowValidationException()
         {
-            FilePathValidator filePathValidator = new FilePathValidator();
+            FilePathValidator validator = new FilePathValidator();
 
-            filePathValidator.Validate(@"C:\My Documents\Shopping\MyDoc.docx.");
+            validator.Validate(OSConvert(@"C:\My Documents \Shopping\MyDoc.docx"));
         }
 
         [TestMethod]
         [ExpectedException(typeof(ValidationException))]
-        public void Validate_InvalidFilePath3_ShouldThrowValidationException()
+        public void Validate_InvalidFilePath_FileNameEndsWithDot_ShouldThrowValidationException()
         {
-            FilePathValidator filePathValidator = new FilePathValidator();
+            FilePathValidator validator = new FilePathValidator();
 
-            filePathValidator.Validate(@"C:\My Documents \Shopping\MyDoc.docx.");
+            validator.Validate(OSConvert(@"C:\My Documents\Shopping\MyDoc.docx."));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
+        public void Validate_InvalidFilePath_FileNameEndsWithSpace_ShouldThrowValidationException()
+        {
+            FilePathValidator validator = new FilePathValidator();
+
+            validator.Validate(OSConvert(@"C:\My Documents\Shopping\MyDoc.docx "));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
+        public void Validate_InvalidFilePath_IllegalCharacterInDirectoryName_ShouldThrowValidationException()
+        {
+            FilePathValidator validator = new FilePathValidator();
+
+            validator.Validate(OSConvert(@"C:\My DocumenXts\Shopping\MyDoc.docx"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
+        public void Validate_InvalidFilePath_IllegalCharacterInFileName_ShouldThrowValidationException()
+        {
+            FilePathValidator validator = new FilePathValidator();
+
+            validator.Validate(OSConvert(@"C:\My Documents\Shopping\MyDXoc.docx"));
         }
 
         [TestMethod]
         public void Validate_ValidFilePath1_ShouldReturn()
         {
-            FilePathValidator filePathValidator = new FilePathValidator();
+            FilePathValidator validator = new FilePathValidator();
 
-            filePathValidator.Validate(@"C:\My Documents\Shopping");
+            validator.Validate(OSConvert(@"C:\My Documents\Shopping"));
         }
 
         [TestMethod]
         public void Validate_ValidFilePath2_ShouldReturn()
         {
-            FilePathValidator filePathValidator = new FilePathValidator();
+            FilePathValidator validator = new FilePathValidator();
 
-            filePathValidator.Validate(@"C:\My Documents\Shopping\MyDoc.docx");
+            validator.Validate(OSConvert(@"C:\My Documents\Shopping\MyDoc.docx"));
         }
 
         [TestMethod]
         public void Validate_ValidFilePath3_ShouldReturn()
         {
-            FilePathValidator filePathValidator = new FilePathValidator();
+            FilePathValidator validator = new FilePathValidator();
 
-            filePathValidator.Validate(@"C:\My Documents\.\Shopping\MyDoc.docx");
+            validator.Validate(OSConvert(@"C:\My Documents\.\Shopping\MyDoc.docx"));
         }
 
         [TestMethod]
         public void Validate_ValidFilePath4_ShouldReturn()
         {
-            FilePathValidator filePathValidator = new FilePathValidator();
+            FilePathValidator validator = new FilePathValidator();
 
-            filePathValidator.Validate(@"C:\My Documents\..\Shopping\MyDoc.docx");
+            validator.Validate(OSConvert(@"C:\My Documents\..\Shopping\MyDoc.docx"));
+        }
+
+        [TestMethod]
+        public void Validate_ValidFilePath5_ShouldReturn()
+        {
+            FilePathValidator validator = new FilePathValidator();
+
+            validator.Validate(OSConvert(@"MyDoc.docx"));
+        }
+
+        [TestMethod]
+        public void Validate_ValidFilePath6_ShouldReturn()
+        {
+            FilePathValidator validator = new FilePathValidator();
+
+            validator.Validate(OSConvert(@"MyDoc"));
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static string OSConvert(string path)
+        {
+            if (OSInfo.IsWindows)
+                path = path.Replace('X', '<');
+            else
+            {
+                path = path.Replace("C:", "");
+
+                path = path.Replace('/', Path.DirectorySeparatorChar);
+
+                path = path.Replace('X', '\0');
+            }
+
+            return(path);
         }
 
         #endregion
