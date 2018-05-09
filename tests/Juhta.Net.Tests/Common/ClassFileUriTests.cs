7d2e0862c3e15d426f63a4327c313f7b43721815
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-05-09 19:55:21 +0300

    ClassFileUri tests refactored and added

--- tests/Juhta.Net.Tests/Common/ClassFileUriTests.cs
+++ tests/Juhta.Net.Tests/Common/ClassFileUriTests.cs
@@ -6,20 +6,40 @@ using System;
 namespace Juhta.Net.Tests.Common
 {
     [TestClass]
-    public class ClassFileUriTests
+    public class ClassFileUriTests : TestClassBase
     {
         #region Test Methods
 
         [TestMethod]
-        public void NewInstance_ValidValue_FragmentStartsWithDot_ShouldReturn()
+        public void NewInstance_ValidValue_Directory_ClassNamespace_ShouldReturn()
         {
             ClassFileUri classFileUri;
 
-            classFileUri = new ClassFileUri("C:\\Temp\\MyLibrary.dll#.MyClass");
+            classFileUri = new ClassFileUri(ToOSPath("C:\\Temp\\MyLibrary.dll#MyFirstClasses.Shared.MyClass"));
 
-            Assert.AreEqual<string>("C:\\Temp\\MyLibrary.dll", classFileUri.LibraryFilePath);
+            Assert.AreEqual<string>(ToOSPath("C:\\Temp\\MyLibrary.dll"), classFileUri.LibraryFilePath);
 
-            Assert.AreEqual<string>("C:\\Temp", classFileUri.LibraryDirectory);
+            Assert.AreEqual<string>(ToOSPath("C:\\Temp"), classFileUri.LibraryDirectory);
+
+            Assert.AreEqual<string>("MyLibrary.dll", classFileUri.LibraryFileName);
+
+            Assert.AreEqual<string>("MyFirstClasses.Shared.MyClass", classFileUri.FullClassName);
+
+            Assert.AreEqual<string>("MyFirstClasses.Shared", classFileUri.ClassNamespace);
+
+            Assert.AreEqual<string>("MyClass", classFileUri.ClassName);
+        }
+
+        [TestMethod]
+        public void NewInstance_ValidValue_Directory_DefaultClassNamespace_ShouldReturn()
+        {
+            ClassFileUri classFileUri;
+
+            classFileUri = new ClassFileUri(ToOSPath("C:\\Temp\\MyLibrary.dll#.MyClass"));
+
+            Assert.AreEqual<string>(ToOSPath("C:\\Temp\\MyLibrary.dll"), classFileUri.LibraryFilePath);
+
+            Assert.AreEqual<string>(ToOSPath("C:\\Temp"), classFileUri.LibraryDirectory);
 
             Assert.AreEqual<string>("MyLibrary.dll", classFileUri.LibraryFileName);
 
@@ -31,15 +51,115 @@ namespace Juhta.Net.Tests.Common
         }
 
         [TestMethod]
-        public void NewInstance_ValidValue_NoDirectory_FragmentStartsWithDot_ShouldReturn()
+        public void NewInstance_ValidValue_Directory_NoClassNamespace_ShouldReturn()
+        {
+            ClassFileUri classFileUri;
+
+            classFileUri = new ClassFileUri(ToOSPath("C:\\Temp\\MyLibrary.dll#MyFirstClass"));
+
+            Assert.AreEqual<string>(ToOSPath("C:\\Temp\\MyLibrary.dll"), classFileUri.LibraryFilePath);
+
+            Assert.AreEqual<string>(ToOSPath("C:\\Temp"), classFileUri.LibraryDirectory);
+
+            Assert.AreEqual<string>("MyLibrary.dll", classFileUri.LibraryFileName);
+
+            Assert.AreEqual<string>("MyFirstClass", classFileUri.FullClassName);
+
+            Assert.AreEqual<string>(null, classFileUri.ClassNamespace);
+
+            Assert.AreEqual<string>("MyFirstClass", classFileUri.ClassName);
+        }
+
+        [TestMethod]
+        public void NewInstance_ValidValue_FileScheme_Directory_ClassNamespace_ShouldReturn()
+        {
+            ClassFileUri classFileUri;
+
+            classFileUri = new ClassFileUri(ToOSPath("file:///C:\\MyApp\\MyLibrary.dll#MyFirstClasses.Shared.Web.MyClass"));
+
+            Assert.AreEqual<string>(ToOSPath("C:\\MyApp\\MyLibrary.dll"), classFileUri.LibraryFilePath);
+
+            Assert.AreEqual<string>(ToOSPath("C:\\MyApp"), classFileUri.LibraryDirectory);
+
+            Assert.AreEqual<string>("MyLibrary.dll", classFileUri.LibraryFileName);
+
+            Assert.AreEqual<string>("MyFirstClasses.Shared.Web.MyClass", classFileUri.FullClassName);
+
+            Assert.AreEqual<string>("MyFirstClasses.Shared.Web", classFileUri.ClassNamespace);
+
+            Assert.AreEqual<string>("MyClass", classFileUri.ClassName);
+        }
+
+        [TestMethod]
+        public void NewInstance_ValidValue_FileScheme_NoDirectory_ClassNamespace_ShouldReturn()
+        {
+            ClassFileUri classFileUri;
+
+            classFileUri = new ClassFileUri(ToOSPath("file:///MyLibrary.dll#MyFirstClasses.Shared.Web.MyClass"));
+
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory + "\\MyLibrary.dll"), classFileUri.LibraryFilePath);
+
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory), classFileUri.LibraryDirectory);
+
+            Assert.AreEqual<string>("MyLibrary.dll", classFileUri.LibraryFileName);
+
+            Assert.AreEqual<string>("MyFirstClasses.Shared.Web.MyClass", classFileUri.FullClassName);
+
+            Assert.AreEqual<string>("MyFirstClasses.Shared.Web", classFileUri.ClassNamespace);
+
+            Assert.AreEqual<string>("MyClass", classFileUri.ClassName);
+        }
+
+        [TestMethod]
+        public void NewInstance_ValidValue_FileScheme_NoDirectory_NoClassNamespace_ShouldReturn()
+        {
+            ClassFileUri classFileUri;
+
+            classFileUri = new ClassFileUri(ToOSPath("file:///MyLibrary.dll#MyFirstClass"));
+
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory + "\\MyLibrary.dll"), classFileUri.LibraryFilePath);
+
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory), classFileUri.LibraryDirectory);
+
+            Assert.AreEqual<string>("MyLibrary.dll", classFileUri.LibraryFileName);
+
+            Assert.AreEqual<string>("MyFirstClass", classFileUri.FullClassName);
+
+            Assert.AreEqual<string>(null, classFileUri.ClassNamespace);
+
+            Assert.AreEqual<string>("MyFirstClass", classFileUri.ClassName);
+        }
+
+        [TestMethod]
+        public void NewInstance_ValidValue_NoDirectory_ClassNamespace_ShouldReturn()
+        {
+            ClassFileUri classFileUri;
+
+            classFileUri = new ClassFileUri(ToOSPath("MyLibrary.dll#MyFirstClasses.Shared.Web.MyClass"));
+
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory + "\\MyLibrary.dll"), classFileUri.LibraryFilePath);
+
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory), classFileUri.LibraryDirectory);
+
+            Assert.AreEqual<string>("MyLibrary.dll", classFileUri.LibraryFileName);
+
+            Assert.AreEqual<string>("MyFirstClasses.Shared.Web.MyClass", classFileUri.FullClassName);
+
+            Assert.AreEqual<string>("MyFirstClasses.Shared.Web", classFileUri.ClassNamespace);
+
+            Assert.AreEqual<string>("MyClass", classFileUri.ClassName);
+        }
+
+        [TestMethod]
+        public void NewInstance_ValidValue_NoDirectory_DefaultClassNamespace_ShouldReturn()
         {
             ClassFileUri classFileUri;
 
             classFileUri = new ClassFileUri("MyLibrary.dll#.MyClass");
 
-            Assert.AreEqual<string>(Environment.CurrentDirectory + "\\MyLibrary.dll", classFileUri.LibraryFilePath);
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory + "\\MyLibrary.dll"), classFileUri.LibraryFilePath);
 
-            Assert.AreEqual<string>(Environment.CurrentDirectory, classFileUri.LibraryDirectory);
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory), classFileUri.LibraryDirectory);
 
             Assert.AreEqual<string>("MyLibrary.dll", classFileUri.LibraryFileName);
 
@@ -57,9 +177,9 @@ namespace Juhta.Net.Tests.Common
 
             classFileUri = new ClassFileUri("MyLibrary.dll#MyClass");
 
-            Assert.AreEqual<string>(Environment.CurrentDirectory + "\\MyLibrary.dll", classFileUri.LibraryFilePath);
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory + "\\MyLibrary.dll"), classFileUri.LibraryFilePath);
 
-            Assert.AreEqual<string>(Environment.CurrentDirectory, classFileUri.LibraryDirectory);
+            Assert.AreEqual<string>(ToOSPath(Environment.CurrentDirectory), classFileUri.LibraryDirectory);
 
             Assert.AreEqual<string>("MyLibrary.dll", classFileUri.LibraryFileName);
 
