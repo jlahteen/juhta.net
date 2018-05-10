02806fa09207d6cc71c5ff5e365aeea0022724e9
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-05-10 19:37:07 +0300

    ClassFileUri tests added

--- tests/Juhta.Net.Tests/Common/ClassFileUriTests.cs
+++ tests/Juhta.Net.Tests/Common/ClassFileUriTests.cs
@@ -11,6 +11,162 @@ namespace Juhta.Net.Tests.Common
         #region Test Methods
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewInstance_InvalidValue_EmptyFragmentPart_ShouldThrowArgumentException()
+        {
+            ClassFileUri classFileUri;
+
+            try
+            {
+                classFileUri = new ClassFileUri("file:///" + ToOSPath("C:\\Temp\\MyLibrary.dll#"));
+            }
+
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message.StartsWith("[Juhta.Net.Error10034]"));
+
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewInstance_InvalidValue_InvalidFragmentPart_ShouldThrowArgumentException()
+        {
+            ClassFileUri classFileUri;
+
+            try
+            {
+                classFileUri = new ClassFileUri("file:///" + ToOSPath("C:\\Temp\\MyLibrary.dll#Class-Name"));
+            }
+
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message.StartsWith("[Juhta.Net.Error10036]"));
+
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewInstance_InvalidValue_InvalidLibraryDirectory_ShouldThrowArgumentException()
+        {
+            ClassFileUri classFileUri;
+
+            try
+            {
+                classFileUri = new ClassFileUri("file:///" + ToOSPath("C:\\TempX\\MyLibrary.dll#Class"));
+            }
+
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message.StartsWith("[Juhta.Net.Error10039]"));
+
+                Assert.IsTrue(ex.InnerException.Message.StartsWith("[Juhta.Net.Error10038]"));
+
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewInstance_InvalidValue_InvalidLibraryFileName_ShouldThrowArgumentException()
+        {
+            ClassFileUri classFileUri;
+
+            try
+            {
+                classFileUri = new ClassFileUri("file:///" + ToOSPath("C:\\Temp\\MyLibXrary.dll#Class"));
+            }
+
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message.StartsWith("[Juhta.Net.Error10039]"));
+
+                Assert.IsTrue(ex.InnerException.Message.StartsWith("[Juhta.Net.Error10038]"));
+
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewInstance_InvalidValue_InvalidLibraryFileType_ShouldThrowArgumentException()
+        {
+            ClassFileUri classFileUri;
+
+            try
+            {
+                classFileUri = new ClassFileUri("file:///" + ToOSPath("C:\\Temp\\MyLibrary.bin#Class"));
+            }
+
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message.StartsWith("[Juhta.Net.Error10035]"));
+
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewInstance_InvalidValue_LocalhostFileUri_ShouldThrowArgumentException()
+        {
+            ClassFileUri classFileUri;
+
+            try
+            {
+                classFileUri = new ClassFileUri("file://" + ToOSPath("C:\\Temp\\MyLibrary.dll#MyFirstClasses.Shared.MyClass"));
+            }
+
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message.StartsWith("[Juhta.Net.Error10048]"));
+
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewInstance_InvalidValue_NoFragmentPart_ShouldThrowArgumentException()
+        {
+            ClassFileUri classFileUri;
+
+            try
+            {
+                classFileUri = new ClassFileUri("file:///" + ToOSPath("C:\\Temp\\MyLibrary.dll"));
+            }
+
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message.StartsWith("[Juhta.Net.Error10034]"));
+
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewInstance_InvalidValue_NonFileScheme_ShouldThrowArgumentException()
+        {
+            ClassFileUri classFileUri;
+
+            try
+            {
+                classFileUri = new ClassFileUri("http://" + ToOSPath("C:\\Temp\\MyLibrary.dll#MyFirstClasses.Shared.MyClass"));
+            }
+
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message.StartsWith("[Juhta.Net.Error10033]"));
+
+                throw;
+            }
+        }
+
+        [TestMethod]
         public void NewInstance_ValidValue_Directory_ClassNamespace_ShouldReturn()
         {
             ClassFileUri classFileUri;
