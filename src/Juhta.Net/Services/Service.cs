0c8dc884373f698601ac980164943850a6ed88be
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-06-02 00:00:57 +0300

    New ClassId constructor applied in Service

--- src/Juhta.Net/Services/Service.cs
+++ src/Juhta.Net/Services/Service.cs
@@ -88,7 +88,7 @@ namespace Juhta.Net.Services
 
             try
             {
-                m_classId = new ClassId(Application.Instance.BinDirectory + Path.DirectorySeparatorChar + serviceNode.GetAttribute("classId"));
+                m_classId = new ClassId(serviceNode.GetAttribute("classId"), Application.Instance.BinDirectory);
 
                 constructorParamsNode = serviceNode.SelectSingleNode("ns:constructorParams", namespaceManager);
 
