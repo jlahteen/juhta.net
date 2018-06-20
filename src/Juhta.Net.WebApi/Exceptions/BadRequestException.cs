35ca55cb5dfbe591ff7a577e039dc4b0db29a99f
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-06-20 21:41:11 +0300

    Juhta.Net.WebApi library added to the solution

---
+++ src/Juhta.Net.WebApi/Exceptions/BadRequestException.cs
@@ -0,0 +1,35 @@
+﻿
+//
+// Juhta.NET, Copyright (c) 2017 Juha Lähteenmäki
+//
+// This source code may be used, modified and distributed under the terms of
+// the MIT license. Please refer to the LICENSE.txt file for details.
+//
+
+using System.Net;
+
+namespace Juhta.Net.WebApi.Exceptions
+{
+    /// <summary>
+    /// Defines an exception class for the HTTP error Bad Request.
+    /// </summary>
+    public class BadRequestException : ClientFaultException
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public BadRequestException() : base(HttpStatusCode.BadRequest)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="message">Specifies an error message.</param>
+        public BadRequestException(string message) : base(HttpStatusCode.BadRequest, message)
+        {}
+
+        #endregion
+    }
+}
