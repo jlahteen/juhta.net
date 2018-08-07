be428c21646dc35b142e0d900c79c5a2c580ce8c
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-08-07 19:10:46 +0300

    Minor change in the implementation of FrameworkInfo.RootNamespace

--- src/Juhta.Net/FrameworkInfo.cs
+++ src/Juhta.Net/FrameworkInfo.cs
@@ -1,6 +1,6 @@
 ﻿
 //
-// Juhta.NET, Copyright (c) 2017 Juha Lähteenmäki
+// Juhta.NET, Copyright (c) 2017-2018 Juha Lähteenmäki
 //
 // This source code may be used, modified and distributed under the terms of
 // the MIT license. Please refer to the LICENSE.txt file for details.
@@ -47,7 +47,7 @@ namespace Juhta.Net
         /// </summary>
         public static string RootNamespace
         {
-            get {return(typeof(FrameworkInfo).Namespace);}
+            get {return(typeof(LibraryMessages).Namespace);}
         }
 
         #endregion
