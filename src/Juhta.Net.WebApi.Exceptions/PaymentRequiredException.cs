2493d47748a7c1789cbbeac6a018b76e341a0889
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-09-07 21:17:02 +0300

    Missing client error exception classes added

---
+++ src/Juhta.Net.WebApi.Exceptions/PaymentRequiredException.cs
@@ -0,0 +1,35 @@
+﻿
+//
+// Juhta.NET, Copyright (c) 2017-2018 Juha Lähteenmäki
+//
+// This source code may be used, modified and distributed under the terms of
+// the MIT license. Please refer to the LICENSE.txt file for details.
+//
+
+using System.Net;
+
+namespace Juhta.Net.WebApi.Exceptions
+{
+    /// <summary>
+    /// Defines an exception class for the HTTP error Payment Required.
+    /// </summary>
+    public class PaymentRequiredException : ClientErrorException
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public PaymentRequiredException() : base(HttpStatusCode.PaymentRequired)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="message">Specifies an error message.</param>
+        public PaymentRequiredException(string message) : base(HttpStatusCode.PaymentRequired, message)
+        {}
+
+        #endregion
+    }
+}
