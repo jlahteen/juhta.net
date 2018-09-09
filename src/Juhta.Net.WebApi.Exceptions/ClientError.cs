39e5718be2eb609a23d11e16c1cbaafda4edf274
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-09-09 15:56:46 +0300

    WebApiError, ClientError and ServerError added

---
+++ src/Juhta.Net.WebApi.Exceptions/ClientError.cs
@@ -0,0 +1,25 @@
+﻿
+//
+// Juhta.NET, Copyright (c) 2017-2018 Juha Lähteenmäki
+//
+// This source code may be used, modified and distributed under the terms of
+// the MIT license. Please refer to the LICENSE.txt file for details.
+//
+
+namespace Juhta.Net.WebApi.Exceptions
+{
+    /// <summary>
+    /// Defines a class for serializing instances of <see cref="ClientErrorException"/>.
+    /// </summary>
+    public abstract class ClientError : WebApiError
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the custom error identifier of the client error.
+        /// </summary>
+        public string ErrorId {get; set;}
+
+        #endregion
+    }
+}
