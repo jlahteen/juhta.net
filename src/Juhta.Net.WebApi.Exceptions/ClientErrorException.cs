a9ec8bdae296d239e7bcb7f146ec222941466624
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-09-10 19:16:40 +0300

    ClientError and ClientErrorException moved to the root namespace of the library

---
+++ src/Juhta.Net.WebApi.Exceptions/ClientErrorException.cs
@@ -0,0 +1,37 @@
+﻿
+//
+// Juhta.NET, Copyright (c) 2017-2018 Juha Lähteenmäki
+//
+// This source code may be used, modified and distributed under the terms of
+// the MIT license. Please refer to the LICENSE.txt file for details.
+//
+
+using System.Net;
+
+namespace Juhta.Net.WebApi.Exceptions
+{
+    /// <summary>
+    /// Defines an abstract base class for the client error Web API exceptions.
+    /// </summary>
+    public abstract class ClientErrorException : WebApiException
+    {
+        #region Protected Constructors
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="httpStatusCode">Specifies an HTTP status code.</param>
+        protected ClientErrorException(HttpStatusCode httpStatusCode) : base(httpStatusCode)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="httpStatusCode">Specifies an HTTP status code.</param>
+        /// <param name="message">Specifies an error message.</param>
+        protected ClientErrorException(HttpStatusCode httpStatusCode, string message) : base(httpStatusCode, message)
+        {}
+
+        #endregion
+    }
+}
