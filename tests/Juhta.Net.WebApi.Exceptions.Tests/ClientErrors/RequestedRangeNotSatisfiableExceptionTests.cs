03f411c748f7dfc8b02481080a4bdd1afbc84d84
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-11-28 19:11:07 +0200

    Unit tests for RequestedRangeNotSatisfiableException added

---
+++ tests/Juhta.Net.WebApi.Exceptions.Tests/ClientErrors/RequestedRangeNotSatisfiableExceptionTests.cs
@@ -0,0 +1,184 @@
+﻿
+using Juhta.Net.WebApi.Exceptions.ClientErrors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Juhta.Net.WebApi.Exceptions.Tests.ClientErrors
+{
+    [TestClass]
+    public class RequestedRangeNotSatisfiableExceptionTests : WebApiExceptionTests
+    {
+        #region Test Methods
+
+        [TestMethod]
+        public void ThrowAndSerialize_RequestedRangeNotSatisfiableException1_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            RequestedRangeNotSatisfiableException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new RequestedRangeNotSatisfiableException();
+            }
+
+            catch (RequestedRangeNotSatisfiableException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_RequestedRangeNotSatisfiableException1_ShouldReturn",
+                    null,
+                    "ClientError." + HttpStatusCode.RequestedRangeNotSatisfiable.ToString(),
+                    "Exception of type 'Juhta.Net.WebApi.Exceptions.ClientErrors.RequestedRangeNotSatisfiableException' was thrown.",
+                    HttpStatusCode.RequestedRangeNotSatisfiable
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (RequestedRangeNotSatisfiableException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_RequestedRangeNotSatisfiableException2_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            RequestedRangeNotSatisfiableException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new RequestedRangeNotSatisfiableException(MyApiError.InvalidOrderNumber);
+            }
+
+            catch (RequestedRangeNotSatisfiableException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_RequestedRangeNotSatisfiableException2_ShouldReturn",
+                    MyApiError.InvalidOrderNumber.ToString(),
+                    "ClientError." + HttpStatusCode.RequestedRangeNotSatisfiable.ToString(),
+                    "Exception of type 'Juhta.Net.WebApi.Exceptions.ClientErrors.RequestedRangeNotSatisfiableException' was thrown.",
+                    HttpStatusCode.RequestedRangeNotSatisfiable
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (RequestedRangeNotSatisfiableException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_RequestedRangeNotSatisfiableException3_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            RequestedRangeNotSatisfiableException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new RequestedRangeNotSatisfiableException("RequestedRangeNotSatisfiableException Specified order number is invalid.");
+            }
+
+            catch (RequestedRangeNotSatisfiableException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_RequestedRangeNotSatisfiableException3_ShouldReturn",
+                    null,
+                    "ClientError." + HttpStatusCode.RequestedRangeNotSatisfiable.ToString(),
+                    "RequestedRangeNotSatisfiableException Specified order number is invalid.",
+                    HttpStatusCode.RequestedRangeNotSatisfiable
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (RequestedRangeNotSatisfiableException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_RequestedRangeNotSatisfiableException4_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            RequestedRangeNotSatisfiableException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new RequestedRangeNotSatisfiableException(MyApiError.InvalidOrderNumber, "RequestedRangeNotSatisfiableException Specified order number is invalid.");
+            }
+
+            catch (RequestedRangeNotSatisfiableException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_RequestedRangeNotSatisfiableException4_ShouldReturn",
+                    MyApiError.InvalidOrderNumber.ToString(),
+                    "ClientError." + HttpStatusCode.RequestedRangeNotSatisfiable.ToString(),
+                    "RequestedRangeNotSatisfiableException Specified order number is invalid.",
+                    HttpStatusCode.RequestedRangeNotSatisfiable
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (RequestedRangeNotSatisfiableException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        #endregion
+    }
+}
