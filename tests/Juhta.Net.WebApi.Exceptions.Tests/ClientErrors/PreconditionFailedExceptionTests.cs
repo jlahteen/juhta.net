d1de35e9c91a184fbdb6779302b54b2cd79ec380
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-11-28 19:09:58 +0200

    Unit tests for PreconditionFailedException added

---
+++ tests/Juhta.Net.WebApi.Exceptions.Tests/ClientErrors/PreconditionFailedExceptionTests.cs
@@ -0,0 +1,184 @@
+﻿
+using Juhta.Net.WebApi.Exceptions.ClientErrors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Juhta.Net.WebApi.Exceptions.Tests.ClientErrors
+{
+    [TestClass]
+    public class PreconditionFailedExceptionTests : WebApiExceptionTests
+    {
+        #region Test Methods
+
+        [TestMethod]
+        public void ThrowAndSerialize_PreconditionFailedException1_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            PreconditionFailedException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new PreconditionFailedException();
+            }
+
+            catch (PreconditionFailedException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_PreconditionFailedException1_ShouldReturn",
+                    null,
+                    "ClientError." + HttpStatusCode.PreconditionFailed.ToString(),
+                    "Exception of type 'Juhta.Net.WebApi.Exceptions.ClientErrors.PreconditionFailedException' was thrown.",
+                    HttpStatusCode.PreconditionFailed
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (PreconditionFailedException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_PreconditionFailedException2_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            PreconditionFailedException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new PreconditionFailedException(MyApiError.InvalidOrderNumber);
+            }
+
+            catch (PreconditionFailedException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_PreconditionFailedException2_ShouldReturn",
+                    MyApiError.InvalidOrderNumber.ToString(),
+                    "ClientError." + HttpStatusCode.PreconditionFailed.ToString(),
+                    "Exception of type 'Juhta.Net.WebApi.Exceptions.ClientErrors.PreconditionFailedException' was thrown.",
+                    HttpStatusCode.PreconditionFailed
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (PreconditionFailedException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_PreconditionFailedException3_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            PreconditionFailedException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new PreconditionFailedException("PreconditionFailedException Specified order number is invalid.");
+            }
+
+            catch (PreconditionFailedException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_PreconditionFailedException3_ShouldReturn",
+                    null,
+                    "ClientError." + HttpStatusCode.PreconditionFailed.ToString(),
+                    "PreconditionFailedException Specified order number is invalid.",
+                    HttpStatusCode.PreconditionFailed
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (PreconditionFailedException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_PreconditionFailedException4_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            PreconditionFailedException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new PreconditionFailedException(MyApiError.InvalidOrderNumber, "PreconditionFailedException Specified order number is invalid.");
+            }
+
+            catch (PreconditionFailedException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_PreconditionFailedException4_ShouldReturn",
+                    MyApiError.InvalidOrderNumber.ToString(),
+                    "ClientError." + HttpStatusCode.PreconditionFailed.ToString(),
+                    "PreconditionFailedException Specified order number is invalid.",
+                    HttpStatusCode.PreconditionFailed
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (PreconditionFailedException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        #endregion
+    }
+}
