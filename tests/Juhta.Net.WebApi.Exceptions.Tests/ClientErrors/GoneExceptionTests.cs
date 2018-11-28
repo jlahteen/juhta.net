6b139fa34083341fa9f3a8b64f80981d1c9d6beb
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-11-28 18:48:21 +0200

    Unit tests for GoneException added

---
+++ tests/Juhta.Net.WebApi.Exceptions.Tests/ClientErrors/GoneExceptionTests.cs
@@ -0,0 +1,184 @@
+﻿
+using Juhta.Net.WebApi.Exceptions.ClientErrors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Juhta.Net.WebApi.Exceptions.Tests.ClientErrors
+{
+    [TestClass]
+    public class GoneExceptionTests : WebApiExceptionTests
+    {
+        #region Test Methods
+
+        [TestMethod]
+        public void ThrowAndSerialize_GoneException1_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            GoneException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new GoneException();
+            }
+
+            catch (GoneException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_GoneException1_ShouldReturn",
+                    null,
+                    "ClientError." + HttpStatusCode.Gone.ToString(),
+                    "Exception of type 'Juhta.Net.WebApi.Exceptions.ClientErrors.GoneException' was thrown.",
+                    HttpStatusCode.Gone
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (GoneException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_GoneException2_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            GoneException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new GoneException(MyApiError.InvalidOrderNumber);
+            }
+
+            catch (GoneException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_GoneException2_ShouldReturn",
+                    MyApiError.InvalidOrderNumber.ToString(),
+                    "ClientError." + HttpStatusCode.Gone.ToString(),
+                    "Exception of type 'Juhta.Net.WebApi.Exceptions.ClientErrors.GoneException' was thrown.",
+                    HttpStatusCode.Gone
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (GoneException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_GoneException3_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            GoneException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new GoneException("GoneException Specified order number is invalid.");
+            }
+
+            catch (GoneException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_GoneException3_ShouldReturn",
+                    null,
+                    "ClientError." + HttpStatusCode.Gone.ToString(),
+                    "GoneException Specified order number is invalid.",
+                    HttpStatusCode.Gone
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (GoneException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_GoneException4_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            GoneException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new GoneException(MyApiError.InvalidOrderNumber, "GoneException Specified order number is invalid.");
+            }
+
+            catch (GoneException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_GoneException4_ShouldReturn",
+                    MyApiError.InvalidOrderNumber.ToString(),
+                    "ClientError." + HttpStatusCode.Gone.ToString(),
+                    "GoneException Specified order number is invalid.",
+                    HttpStatusCode.Gone
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (GoneException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        #endregion
+    }
+}
