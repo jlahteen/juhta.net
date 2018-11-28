f64947060742919f18c53cd918a401c3b826ef58
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2018-11-28 18:49:45 +0200

    Unit tests for MethodNotAllowedException added

---
+++ tests/Juhta.Net.WebApi.Exceptions.Tests/ClientErrors/MethodNotAllowedExceptionTests.cs
@@ -0,0 +1,184 @@
+﻿
+using Juhta.Net.WebApi.Exceptions.ClientErrors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Juhta.Net.WebApi.Exceptions.Tests.ClientErrors
+{
+    [TestClass]
+    public class MethodNotAllowedExceptionTests : WebApiExceptionTests
+    {
+        #region Test Methods
+
+        [TestMethod]
+        public void ThrowAndSerialize_MethodNotAllowedException1_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            MethodNotAllowedException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new MethodNotAllowedException();
+            }
+
+            catch (MethodNotAllowedException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_MethodNotAllowedException1_ShouldReturn",
+                    null,
+                    "ClientError." + HttpStatusCode.MethodNotAllowed.ToString(),
+                    "Exception of type 'Juhta.Net.WebApi.Exceptions.ClientErrors.MethodNotAllowedException' was thrown.",
+                    HttpStatusCode.MethodNotAllowed
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (MethodNotAllowedException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_MethodNotAllowedException2_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            MethodNotAllowedException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new MethodNotAllowedException(MyApiError.InvalidOrderNumber);
+            }
+
+            catch (MethodNotAllowedException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_MethodNotAllowedException2_ShouldReturn",
+                    MyApiError.InvalidOrderNumber.ToString(),
+                    "ClientError." + HttpStatusCode.MethodNotAllowed.ToString(),
+                    "Exception of type 'Juhta.Net.WebApi.Exceptions.ClientErrors.MethodNotAllowedException' was thrown.",
+                    HttpStatusCode.MethodNotAllowed
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (MethodNotAllowedException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_MethodNotAllowedException3_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            MethodNotAllowedException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new MethodNotAllowedException("MethodNotAllowedException Specified order number is invalid.");
+            }
+
+            catch (MethodNotAllowedException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_MethodNotAllowedException3_ShouldReturn",
+                    null,
+                    "ClientError." + HttpStatusCode.MethodNotAllowed.ToString(),
+                    "MethodNotAllowedException Specified order number is invalid.",
+                    HttpStatusCode.MethodNotAllowed
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (MethodNotAllowedException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_MethodNotAllowedException4_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            MethodNotAllowedException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new MethodNotAllowedException(MyApiError.InvalidOrderNumber, "MethodNotAllowedException Specified order number is invalid.");
+            }
+
+            catch (MethodNotAllowedException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_MethodNotAllowedException4_ShouldReturn",
+                    MyApiError.InvalidOrderNumber.ToString(),
+                    "ClientError." + HttpStatusCode.MethodNotAllowed.ToString(),
+                    "MethodNotAllowedException Specified order number is invalid.",
+                    HttpStatusCode.MethodNotAllowed
+                );
+
+                clientError1 = ex.ToClientError();
+
+                exception1 = ex;
+            }
+
+            clientError2 = JsonConvert.DeserializeObject<ClientError>(JsonConvert.SerializeObject(clientError1));
+
+            try
+            {
+                clientError2.Throw();
+            }
+
+            catch (MethodNotAllowedException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        #endregion
+    }
+}
