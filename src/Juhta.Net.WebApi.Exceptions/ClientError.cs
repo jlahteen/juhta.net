39f6a15a1031cfc53df10db74a8163efcaa441db
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2019-01-03 13:02:39 +0200

    ClientError added

---
+++ src/Juhta.Net.WebApi.Exceptions/ClientError.cs
@@ -0,0 +1,40 @@
+﻿
+//
+// Juhta.NET, Copyright (c) 2017-2019 Juha Lähteenmäki
+//
+// This source code may be used, modified and distributed under the terms of
+// the MIT license. Please refer to the LICENSE.txt file for details.
+//
+
+namespace Juhta.Net.WebApi.Exceptions
+{
+    /// <summary>
+    /// Defines a class for storing information about client errors.
+    /// </summary>
+    public class ClientError
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the identifier code of the client error.
+        /// </summary>
+        public string Code {get; set;}
+
+        /// <summary>
+        /// Gets or sets the field in the incoming request to which the client error relates.
+        /// </summary>
+        public string Field {get; set;}
+
+        /// <summary>
+        /// Gets or sets a URL that provides extra information about the client error.
+        /// </summary>
+        public string HelpUrl {get; set;}
+
+        /// <summary>
+        /// Gets or sets the message of the client error.
+        /// </summary>
+        public string Message {get; set;}
+
+        #endregion
+    }
+}
