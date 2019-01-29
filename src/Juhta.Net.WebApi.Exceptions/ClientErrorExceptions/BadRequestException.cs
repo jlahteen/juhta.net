9eae6376178909f8655ed32abb6675d82167dcd9
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2019-01-29 20:08:05 +0200

    Constructors added to BadRequestException

--- src/Juhta.Net.WebApi.Exceptions/ClientErrorExceptions/BadRequestException.cs
+++ src/Juhta.Net.WebApi.Exceptions/ClientErrorExceptions/BadRequestException.cs
@@ -7,6 +7,7 @@
 //
 
 using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace Juhta.Net.WebApi.Exceptions.ClientErrorExceptions
@@ -27,8 +28,22 @@ namespace Juhta.Net.WebApi.Exceptions.ClientErrorExceptions
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
-        /// <param name="errorCode">Specifies a custom defined error code.</param>
+        /// <param name="clientError">Specifies a client error.</param>
+        public BadRequestException(ClientError clientError) : base(HttpStatusCode.BadRequest, clientError)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
         public BadRequestException(Enum errorCode) : base(HttpStatusCode.BadRequest, null, errorCode.ToString())
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="clientErrors">Specifies a collection of client errors.</param>
+        public BadRequestException(IEnumerable<ClientError> clientErrors) : base(HttpStatusCode.BadRequest, clientErrors)
         {}
 
         /// <summary>
@@ -41,9 +56,53 @@ namespace Juhta.Net.WebApi.Exceptions.ClientErrorExceptions
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
-        /// <param name="errorCode">Specifies a custom defined error code.</param>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="field">Specifies a field to which the error relates.</param>
+        public BadRequestException(Enum errorCode, Enum field) : base(HttpStatusCode.BadRequest, null, errorCode.ToString(), field.ToString())
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
         /// <param name="message">Specifies an error message.</param>
         public BadRequestException(Enum errorCode, string message) : base(HttpStatusCode.BadRequest, message, errorCode.ToString())
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="helpUrl">Specifies a URL that provides extra information about the error.</param>
+        public BadRequestException(string message, string helpUrl) : base(HttpStatusCode.BadRequest, message, null, null, helpUrl)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="field">Specifies a field to which the error relates.</param>
+        /// <param name="message">Specifies an error message.</param>
+        public BadRequestException(Enum errorCode, Enum field, string message) : base(HttpStatusCode.BadRequest, message, errorCode.ToString(), field.ToString())
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="helpUrl">Specifies a URL that provides extra information about the error.</param>
+        public BadRequestException(Enum errorCode, string message, string helpUrl) : base(HttpStatusCode.BadRequest, message, errorCode.ToString(), null, helpUrl)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="field">Specifies a field to which the error relates.</param>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="helpUrl">Specifies a URL that provides extra information about the error.</param>
+        public BadRequestException(Enum errorCode, Enum field, string message, string helpUrl) : base(HttpStatusCode.BadRequest, message, errorCode.ToString(), field.ToString(), helpUrl)
         {}
 
         #endregion
