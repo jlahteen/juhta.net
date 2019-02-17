85d2d7144d87b91eaacc80bebe7e34189df6a4ca
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2019-02-17 15:13:36 +0200

    Unit tests added for ConflictException

--- tests/Juhta.Net.WebApi.Exceptions.Tests/ClientErrors/ConflictExceptionTests.cs
+++ tests/Juhta.Net.WebApi.Exceptions.Tests/ClientErrors/ConflictExceptionTests.cs
@@ -175,6 +175,319 @@ namespace Juhta.Net.WebApi.Exceptions.Tests.ClientErrors
             AssertExceptions(exception1, exception2);
         }
 
+        [TestMethod]
+        public void ThrowAndSerialize_ConflictException5_ShouldReturn()
+        {
+            ClientError clientError;
+            ClientErrorResponse clientErrorResponse1, clientErrorResponse2;
+            ConflictException exception1 = null, exception2 = null;
+
+            try
+            {
+                clientError = new ClientError{Code = "MyApiErrorCode.XYZ", Message = "Error XYZ occurred.", Field = "FormX.FieldY", HelpUrl = "http://juhta.net"};
+
+                throw new ConflictException(clientError);
+            }
+
+            catch (ConflictException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_ConflictException5_ShouldReturn",
+                    HttpStatusCode.Conflict,
+                    "MyApiErrorCode.XYZ",
+                    "Error XYZ occurred.",
+                    "FormX.FieldY",
+                    "http://juhta.net"
+                );
+
+                clientErrorResponse1 = ex.ToClientErrorResponse();
+
+                exception1 = ex;
+            }
+
+            clientErrorResponse2 = JsonConvert.DeserializeObject<ClientErrorResponse>(JsonConvert.SerializeObject(clientErrorResponse1));
+
+            try
+            {
+                clientErrorResponse2.Throw();
+            }
+
+            catch (ConflictException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_ConflictException6_ShouldReturn()
+        {
+            ClientError clientError1, clientError2;
+            ClientErrorResponse clientErrorResponse1, clientErrorResponse2;
+            ConflictException exception1 = null, exception2 = null;
+
+            try
+            {
+                clientError1 = new ClientError{Code = "MyApiErrorCode.XYZ1", Message = "Error XYZ1 occurred.", Field = "FormX.FieldY1", HelpUrl = "http://juhta.net/errorxyz1"};
+
+                clientError2 = new ClientError{Code = "MyApiErrorCode.XYZ2", Message = "Error XYZ2 occurred.", Field = "FormX.FieldY2", HelpUrl = "http://juhta.net/errorxyz2"};
+
+                throw new ConflictException(new ClientError[]{clientError1, clientError2});
+            }
+
+            catch (ConflictException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_ConflictException6_ShouldReturn",
+                    HttpStatusCode.Conflict,
+                    "MyApiErrorCode.XYZ1",
+                    "Error XYZ1 occurred.",
+                    "FormX.FieldY1",
+                    "http://juhta.net/errorxyz1",
+                    "MyApiErrorCode.XYZ2",
+                    "Error XYZ2 occurred.",
+                    "FormX.FieldY2",
+                    "http://juhta.net/errorxyz2"
+                );
+
+                clientErrorResponse1 = ex.ToClientErrorResponse();
+
+                exception1 = ex;
+            }
+
+            clientErrorResponse2 = JsonConvert.DeserializeObject<ClientErrorResponse>(JsonConvert.SerializeObject(clientErrorResponse1));
+
+            try
+            {
+                clientErrorResponse2.Throw();
+            }
+
+            catch (ConflictException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_ConflictException7_ShouldReturn()
+        {
+            ClientErrorResponse clientErrorResponse1, clientErrorResponse2;
+            ConflictException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new ConflictException(ErrorCode.InvalidOrderNumber, Field.CustomerName);
+            }
+
+            catch (ConflictException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_ConflictException7_ShouldReturn",
+                    HttpStatusCode.Conflict,
+                    ErrorCode.InvalidOrderNumber.ToString(),
+                    null,
+                    Field.CustomerName.ToString(),
+                    null
+                );
+
+                clientErrorResponse1 = ex.ToClientErrorResponse();
+
+                exception1 = ex;
+            }
+
+            clientErrorResponse2 = JsonConvert.DeserializeObject<ClientErrorResponse>(JsonConvert.SerializeObject(clientErrorResponse1));
+
+            try
+            {
+                clientErrorResponse2.Throw();
+            }
+
+            catch (ConflictException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_ConflictException8_ShouldReturn()
+        {
+            ClientErrorResponse clientErrorResponse1, clientErrorResponse2;
+            ConflictException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new ConflictException("This is an error, please consult the help URL.", "http://juhta.net/helpurls/353353");
+            }
+
+            catch (ConflictException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_ConflictException8_ShouldReturn",
+                    HttpStatusCode.Conflict,
+                    null,
+                    "This is an error, please consult the help URL.",
+                    null,
+                    "http://juhta.net/helpurls/353353"
+                );
+
+                clientErrorResponse1 = ex.ToClientErrorResponse();
+
+                exception1 = ex;
+            }
+
+            clientErrorResponse2 = JsonConvert.DeserializeObject<ClientErrorResponse>(JsonConvert.SerializeObject(clientErrorResponse1));
+
+            try
+            {
+                clientErrorResponse2.Throw();
+            }
+
+            catch (ConflictException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_ConflictException9_ShouldReturn()
+        {
+            ClientErrorResponse clientErrorResponse1, clientErrorResponse2;
+            ConflictException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new ConflictException(ErrorCode.InvalidOrderNumber, Field.CustomerName, "The field content is not allowed at all. Please do better.");
+            }
+
+            catch (ConflictException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_ConflictException9_ShouldReturn",
+                    HttpStatusCode.Conflict,
+                    ErrorCode.InvalidOrderNumber.ToString(),
+                    "The field content is not allowed at all. Please do better.",
+                    Field.CustomerName.ToString(),
+                    null
+                );
+
+                clientErrorResponse1 = ex.ToClientErrorResponse();
+
+                exception1 = ex;
+            }
+
+            clientErrorResponse2 = JsonConvert.DeserializeObject<ClientErrorResponse>(JsonConvert.SerializeObject(clientErrorResponse1));
+
+            try
+            {
+                clientErrorResponse2.Throw();
+            }
+
+            catch (ConflictException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_ConflictException10_ShouldReturn()
+        {
+            ClientErrorResponse clientErrorResponse1, clientErrorResponse2;
+            ConflictException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new ConflictException(ErrorCode.InvalidOrderNumber, "The field content is not allowed at all. Please do better. At least check out the help URL!", "http://juhta.net/helpurls/1233333");
+            }
+
+            catch (ConflictException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_ConflictException10_ShouldReturn",
+                    HttpStatusCode.Conflict,
+                    ErrorCode.InvalidOrderNumber.ToString(),
+                    "The field content is not allowed at all. Please do better. At least check out the help URL!",
+                    null,
+                    "http://juhta.net/helpurls/1233333"
+                );
+
+                clientErrorResponse1 = ex.ToClientErrorResponse();
+
+                exception1 = ex;
+            }
+
+            clientErrorResponse2 = JsonConvert.DeserializeObject<ClientErrorResponse>(JsonConvert.SerializeObject(clientErrorResponse1));
+
+            try
+            {
+                clientErrorResponse2.Throw();
+            }
+
+            catch (ConflictException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
+        [TestMethod]
+        public void ThrowAndSerialize_ConflictException11_ShouldReturn()
+        {
+            ClientErrorResponse clientErrorResponse1, clientErrorResponse2;
+            ConflictException exception1 = null, exception2 = null;
+
+            try
+            {
+                throw new ConflictException(ErrorCode.InvalidOrderNumber, Field.CustomerName, "The field content is not valid. Please consult the help URL!", "http://juhta.net/helpurls/125533");
+            }
+
+            catch (ConflictException ex)
+            {
+                AssertException(
+                    ex,
+                    "ThrowAndSerialize_ConflictException11_ShouldReturn",
+                    HttpStatusCode.Conflict,
+                    ErrorCode.InvalidOrderNumber.ToString(),
+                    "The field content is not valid. Please consult the help URL!",
+                    Field.CustomerName.ToString(),
+                    "http://juhta.net/helpurls/125533"
+                );
+
+                clientErrorResponse1 = ex.ToClientErrorResponse();
+
+                exception1 = ex;
+            }
+
+            clientErrorResponse2 = JsonConvert.DeserializeObject<ClientErrorResponse>(JsonConvert.SerializeObject(clientErrorResponse1));
+
+            try
+            {
+                clientErrorResponse2.Throw();
+            }
+
+            catch (ConflictException ex)
+            {
+                exception2 = ex;
+            }
+
+            AssertExceptions(exception1, exception2);
+        }
+
         #endregion
     }
 }
