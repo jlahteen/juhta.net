54a61520fd231930f2be1e746b2e7608f08c5419
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2019-02-17 13:25:38 +0200

    Constructors added to ConflictException

--- src/Juhta.Net.WebApi.Exceptions/ClientErrorExceptions/ConflictException.cs
+++ src/Juhta.Net.WebApi.Exceptions/ClientErrorExceptions/ConflictException.cs
@@ -7,6 +7,7 @@
 //
 
 using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace Juhta.Net.WebApi.Exceptions.ClientErrorExceptions
@@ -21,29 +22,87 @@ namespace Juhta.Net.WebApi.Exceptions.ClientErrorExceptions
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
-        public ConflictException() : base(HttpStatusCode.Conflict)
+        public ConflictException() : base(HttpStatusCode.Conflict, null, null, null, null)
         {}
 
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
-        /// <param name="errorCode">Specifies a custom defined error code.</param>
-        public ConflictException(Enum errorCode) : base(HttpStatusCode.Conflict, null, errorCode.ToString())
+        /// <param name="clientError">Specifies a client error.</param>
+        public ConflictException(ClientError clientError) : base(HttpStatusCode.Conflict, clientError)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        public ConflictException(Enum errorCode) : base(HttpStatusCode.Conflict, null, errorCode.ToString(), null, null)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="clientErrors">Specifies a collection of client errors.</param>
+        public ConflictException(IEnumerable<ClientError> clientErrors) : base(HttpStatusCode.Conflict, clientErrors)
         {}
 
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
         /// <param name="message">Specifies an error message.</param>
-        public ConflictException(string message) : base(HttpStatusCode.Conflict, message)
+        public ConflictException(string message) : base(HttpStatusCode.Conflict, message, null, null, null)
         {}
 
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
-        /// <param name="errorCode">Specifies a custom defined error code.</param>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="field">Specifies a field to which the error relates.</param>
+        public ConflictException(Enum errorCode, Enum field) : base(HttpStatusCode.Conflict, null, errorCode.ToString(), field.ToString(), null)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
         /// <param name="message">Specifies an error message.</param>
-        public ConflictException(Enum errorCode, string message) : base(HttpStatusCode.Conflict, message, errorCode.ToString())
+        public ConflictException(Enum errorCode, string message) : base(HttpStatusCode.Conflict, message, errorCode.ToString(), null, null)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="helpUrl">Specifies a URL that provides extra information about the error.</param>
+        public ConflictException(string message, string helpUrl) : base(HttpStatusCode.Conflict, message, null, null, helpUrl)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="field">Specifies a field to which the error relates.</param>
+        /// <param name="message">Specifies an error message.</param>
+        public ConflictException(Enum errorCode, Enum field, string message) : base(HttpStatusCode.Conflict, message, errorCode.ToString(), field.ToString(), null)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="helpUrl">Specifies a URL that provides extra information about the error.</param>
+        public ConflictException(Enum errorCode, string message, string helpUrl) : base(HttpStatusCode.Conflict, message, errorCode.ToString(), null, helpUrl)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="field">Specifies a field to which the error relates.</param>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="helpUrl">Specifies a URL that provides extra information about the error.</param>
+        public ConflictException(Enum errorCode, Enum field, string message, string helpUrl) : base(HttpStatusCode.Conflict, message, errorCode.ToString(), field.ToString(), helpUrl)
         {}
 
         #endregion
