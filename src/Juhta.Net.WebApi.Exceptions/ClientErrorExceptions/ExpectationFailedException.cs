2f0f79473509a23907222f3a58536a69a6abf360
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2019-02-17 13:28:52 +0200

    Constructors added to ExpectationFailedException

--- src/Juhta.Net.WebApi.Exceptions/ClientErrorExceptions/ExpectationFailedException.cs
+++ src/Juhta.Net.WebApi.Exceptions/ClientErrorExceptions/ExpectationFailedException.cs
@@ -7,6 +7,7 @@
 //
 
 using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace Juhta.Net.WebApi.Exceptions.ClientErrorExceptions
@@ -21,29 +22,87 @@ namespace Juhta.Net.WebApi.Exceptions.ClientErrorExceptions
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
-        public ExpectationFailedException() : base(HttpStatusCode.ExpectationFailed)
+        public ExpectationFailedException() : base(HttpStatusCode.ExpectationFailed, null, null, null, null)
         {}
 
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
-        /// <param name="errorCode">Specifies a custom defined error code.</param>
-        public ExpectationFailedException(Enum errorCode) : base(HttpStatusCode.ExpectationFailed, null, errorCode.ToString())
+        /// <param name="clientError">Specifies a client error.</param>
+        public ExpectationFailedException(ClientError clientError) : base(HttpStatusCode.ExpectationFailed, clientError)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        public ExpectationFailedException(Enum errorCode) : base(HttpStatusCode.ExpectationFailed, null, errorCode.ToString(), null, null)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="clientErrors">Specifies a collection of client errors.</param>
+        public ExpectationFailedException(IEnumerable<ClientError> clientErrors) : base(HttpStatusCode.ExpectationFailed, clientErrors)
         {}
 
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
         /// <param name="message">Specifies an error message.</param>
-        public ExpectationFailedException(string message) : base(HttpStatusCode.ExpectationFailed, message)
+        public ExpectationFailedException(string message) : base(HttpStatusCode.ExpectationFailed, message, null, null, null)
         {}
 
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
-        /// <param name="errorCode">Specifies a custom defined error code.</param>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="field">Specifies a field to which the error relates.</param>
+        public ExpectationFailedException(Enum errorCode, Enum field) : base(HttpStatusCode.ExpectationFailed, null, errorCode.ToString(), field.ToString(), null)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
         /// <param name="message">Specifies an error message.</param>
-        public ExpectationFailedException(Enum errorCode, string message) : base(HttpStatusCode.ExpectationFailed, message, errorCode.ToString())
+        public ExpectationFailedException(Enum errorCode, string message) : base(HttpStatusCode.ExpectationFailed, message, errorCode.ToString(), null, null)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="helpUrl">Specifies a URL that provides extra information about the error.</param>
+        public ExpectationFailedException(string message, string helpUrl) : base(HttpStatusCode.ExpectationFailed, message, null, null, helpUrl)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="field">Specifies a field to which the error relates.</param>
+        /// <param name="message">Specifies an error message.</param>
+        public ExpectationFailedException(Enum errorCode, Enum field, string message) : base(HttpStatusCode.ExpectationFailed, message, errorCode.ToString(), field.ToString(), null)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="helpUrl">Specifies a URL that provides extra information about the error.</param>
+        public ExpectationFailedException(Enum errorCode, string message, string helpUrl) : base(HttpStatusCode.ExpectationFailed, message, errorCode.ToString(), null, helpUrl)
+        {}
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="errorCode">Specifies a custom-defined error code.</param>
+        /// <param name="field">Specifies a field to which the error relates.</param>
+        /// <param name="message">Specifies an error message.</param>
+        /// <param name="helpUrl">Specifies a URL that provides extra information about the error.</param>
+        public ExpectationFailedException(Enum errorCode, Enum field, string message, string helpUrl) : base(HttpStatusCode.ExpectationFailed, message, errorCode.ToString(), field.ToString(), helpUrl)
         {}
 
         #endregion
